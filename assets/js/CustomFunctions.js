91584d46fca8a78949316c182a0caac1dba60ec7
Phyrocast <71868443+ComeSerlooten@users.noreply.github.com>
2026-03-28 01:37:33 +0100

    additional imports

---
+++ assets/js/CustomFunctions.js
@@ -0,0 +1,17 @@
+﻿function CopyToClipboardPhone() {
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText("+33695707553");
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText);
+}
+
+function CopyToClipboardEmail() {
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText("c.serloot@gmail.com");
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText);
+}
